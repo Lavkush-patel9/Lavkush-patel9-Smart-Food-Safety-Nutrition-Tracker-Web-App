243962ed6944eb0bca95f46c2567c4457aa64d3c
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2026-04-13 23:49:16 +0530

    Delete Backend/src/hash_pass.js

--- Backend/src/hash_pass.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function run() {
-  const password = "admin123@"; // jo aap login me likh rahe ho
-  const hashed = await bcrypt.hash(password, 10);
-  console.log("Hashed Password:", hashed);
-}
-
-run();
