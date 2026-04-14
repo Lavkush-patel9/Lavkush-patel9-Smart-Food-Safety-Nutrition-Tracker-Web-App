d2d4b66a88a27503aceaea2d1ce113ef81ed0c6c
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2026-04-14 10:37:06 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -33,6 +33,7 @@ app.use("/api/contact", contactRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`✅ Server running on http://127.0.0.1:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
