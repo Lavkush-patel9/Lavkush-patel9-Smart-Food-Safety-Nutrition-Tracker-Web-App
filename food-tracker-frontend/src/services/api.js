888a9f47de944460014ff9e63ac603f99be2f1ed
Lavkush-patel9 <lkush2425@gmail.com>
2026-04-15 01:40:44 +0530

    fix API baseURL for deployment

--- food-tracker-frontend/src/services/api.js
+++ food-tracker-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api"
+  baseURL: "https://lavkush-patel9-smart-food-safety-wd7y.onrender.com/api"
 });
 
 export default API;
